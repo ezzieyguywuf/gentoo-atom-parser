783f9e1a5c76c2c782e3b68295a210dab37e1c70
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-03-03 21:17:51 -0500

    Added data types to Parser module.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,4 +1,36 @@
-module Parser (someFunc) where
+{-# LANGUAGE OverloadedStrings #-}
+module Parser (eitherParse) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- | Base imports
+import Data.Void (Void)
+import Data.Text (Text)
+
+-- | Third-party imports
+import Text.Megaparsec (Parsec)
+
+-- | Our Data definitions
+-------------------------
+type Parser = Parsec Void Text
+
+-- | The fundamental data type that we're trying to parse
+data Atom =
+    Atom { category     :: Text
+         , package_name :: Text
+         , version      :: Text
+         , suffixes     :: [Suffix]
+         , revision     :: Maybe Text
+         } deriving (Show)
+
+-- | Per the gentoo Package Manager Specification, section 3.2
+data Suffix =
+      Alpha Text
+    | Beta  Text
+    | Pre   Text
+    | RC    Text
+    | P     Text
+    deriving (Show)
+
+-- | Function Definitions
+-------------------------
+eitherParse :: Text -> Either Text Atom
+eitherParse _ = Left "Not implemented"
