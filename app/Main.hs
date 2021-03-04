ed44ac1222989ed41ddc2c5e63d9f7141721aa67
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-03-04 15:43:34 -0500

    Tag v0.1, basic parsing works
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Text (Text)
 import Parser (eitherParse, prettyPrintAtom)
 import qualified Data.Text.IO as TextIO
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  putStrLn "Trying to parse dev-lib/foo-1.0"
-  TextIO.putStr $ case eitherParse "dev-lib/foo-1.0" of
+  let packages = [ "dev-lib/foo-1.0"
+                 , "dev-lib/bar-100dpi-2.0"
+                 , "dev-lib/baz-3g"
+                 ]
+  mapM_ runParser packages
+
+runParser :: Text -> IO ()
+runParser text = do
+    TextIO.putStrLn ("Trying to parse " <> text)
+    TextIO.putStr $ case eitherParse text of
                      Left msg -> msg
                      Right atom -> prettyPrintAtom atom
+    putStrLn ""
+    putStrLn ""
--- src/Parser.hs
+++ src/Parser.hs
@@ -10,7 +10,8 @@ import           Data.Void           (Void)
 -- | Third-party imports
 import           Text.Megaparsec      (Parsec, parse, errorBundlePretty
                                       , empty, some, choice, many, optional
-                                      , someTill_, try, sepBy, manyTill)
+                                      , someTill_, try, sepBy1, manyTill, eof
+                                      , (<?>), lookAhead)
 import           Text.Megaparsec.Char (space1, char, alphaNumChar, digitChar
                                       , lowerChar)
 import qualified Text.Megaparsec.Char.Lexer as Lexer
@@ -42,17 +43,17 @@ data Suffix =
 
 -- | Either succesfully parse the input text, or return an helpful error message
 eitherParse :: Text -> Either Text Atom
-eitherParse text = either err Right (parse parseAtom "" text)
+eitherParse text = either err Right (parse (parseAtom <?> "atom") "" text)
     where err = Left . Text.pack . errorBundlePretty
 
 -- | Turns an Atom into pretty text
 prettyPrintAtom :: Atom -> Text
 prettyPrintAtom (Atom category package_name version suffixes maybeRevision) =
-    Text.pack "Category = " <> category <> Text.pack "\n" <>
-    Text.pack "Package Name = " <> package_name <> Text.pack "\n" <>
-    Text.pack "Version = " <> version <> Text.pack "\n" <>
-    Text.pack "suffixes = " <> (Text.intercalate "." . map getSuffix) suffixes <>
-    Text.pack "revision = " <> fromMaybe (Text.pack "r0") maybeRevision
+    Text.pack "    Category     = " <> category <> Text.pack "\n" <>
+    Text.pack "    Package Name = " <> package_name <> Text.pack "\n" <>
+    Text.pack "    Version      = " <> version <> Text.pack "\n" <>
+    Text.pack "    suffixes     = " <> (Text.intercalate "." . map getSuffix) suffixes <> Text.pack "\n" <>
+    Text.pack "    revision     = " <> fromMaybe (Text.pack "r0") maybeRevision
 
 -- | Internal Function Definitions
 -------------------------
@@ -60,10 +61,11 @@ prettyPrintAtom (Atom category package_name version suffixes maybeRevision) =
 -- | Parses a single atom
 parseAtom :: Parser Atom
 parseAtom = do
-    category    <- parseCategory
-    -- packageName <- parseWord
-    -- version <- parseVersionNumber
-    pure (Atom category "not_implemented" "not_implemented" [] Nothing)
+    category    <- parseCategory <?> "category"
+    packageName <- parsePackageName <?> "package name"
+    char '-'
+    version <- parseVersion <?> "version"
+    pure (Atom category packageName version [] Nothing)
 
 -- | Parses the category, which precedes the '/'
 parseCategory :: Parser Text
@@ -72,9 +74,17 @@ parseCategory = do
     moreLetters <- Text.concat <$> manyTill parseAfterChar (char '/')
     pure (firstLetter <> moreLetters)
 
+-- | Parses the package name, whic precedes the '-<version_string>' (notice the
+--   hypen, thanks PMS(?)
+parsePackageName :: Parser Text
+parsePackageName = do
+    firstLetter <- parseFirstChar
+    moreLetters <- (Text.concat <$> manyTill parseAfterChar (lookAhead (char '-' >> parseVersionNumber))) <?> "more letters"
+    pure (firstLetter <> moreLetters)
+
 -- | Parses what PMS allows as the first character in names [A-Za-z0-9_]
 parseFirstChar :: Parser Text
-parseFirstChar = Text.singleton <$> choice [ alphaNumChar, char '_']
+parseFirstChar = Text.singleton <$> choice [ alphaNumChar, char '_'] <?> "first char"
 
 -- | After the first character, the PMS allows these extra characters too
 parseAfterChar :: Parser Text
@@ -82,27 +92,18 @@ parseAfterChar = choice [ parseFirstChar
                         , Text.singleton <$> char '-'
                         , Text.singleton <$> char '.'
                         , Text.singleton <$> char '+'
-                        ]
+                        ] <?> "after char"
+
+parseVersion :: Parser Text
+parseVersion = do
+    versionNumber <- parseVersionNumber
+    suffix        <- maybe "" Text.singleton <$> optional lowerChar
+    pure (versionNumber <> suffix)
 
 -- | Parses the numeric portion of a version
 parseVersionNumber :: Parser Text
-parseVersionNumber = do
-    firstPart <- parseInteger
-    -- many returns zero or more matches
-    backEnd   <- many  (char '.' >> Text.append "." <$> parseInteger)
-    -- empty string or single lower-chase character
-    suffix    <- maybe "" Text.singleton <$> optional lowerChar
-    -- glue it all back together
-    pure (Text.concat (firstPart : (backEnd <> [suffix])))
+parseVersionNumber = Text.intercalate "." <$> sepBy1 parseInteger (Text.singleton <$> char '.')
 
 -- | Parses...an integer
 parseInteger :: Parser Text
 parseInteger = Text.pack <$> some digitChar
-
--- | Used to consume any whitespace
-spaceConsumer :: Parser ()
-spaceConsumer = Lexer.space space1 empty empty
-
--- | Use the given parser to parse a "lexeme", consuming any space after
-lexeme :: Parser a -> Parser a
-lexeme = Lexer.lexeme spaceConsumer
